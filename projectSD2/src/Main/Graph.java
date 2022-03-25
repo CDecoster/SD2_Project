0601b6b6744edb4b8d8311486da37882b34a2302
Axl De clerck <axel.declerck@hotmail.com>
2022-03-25 10:36:38 +0100

    Add : Graph + method

---
+++ projectSD2/src/Main/Graph.java
@@ -0,0 +1,24 @@
+package Main;
+
+import java.io.File;
+
+public class Graph {
+	private File airport;
+	private File fly;
+	
+	public Graph(File airport,File fly) {
+		this.airport = airport;
+		this.fly = fly;
+	}
+	
+	public void calculerItineraireMinimisantNombreVol(String source,String dest) {
+		
+	}
+	
+	public void calculerItineraireMiniminantDistance(String source,String dest) {
+		
+	}
+
+}
+
+
